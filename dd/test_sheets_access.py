d18f00fe4026a6c1c32d441ae3e0909f641e570b
Kajtekcode <118934557+Kajtekcode@users.noreply.github.com>
2025-05-09 20:39:11 +0200

    Delete dd directory

--- dd/test_sheets_access.py
+++
@@ -1,8 +0,0 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-client = gspread.authorize(creds)
-spreadsheet = client.open_by_key('1GrLufeQeZMwP9vd3OYA2zhzh50FOiNFePWtP0PbCCXk')  # Wstaw ARKUSZ_ID
-print("Dostępne zakładki:", [ws.title for ws in spreadsheet.worksheets()])
--- dd/test_venv.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-print("Środowisko wirtualne działa dla Invoice Tracker!")
